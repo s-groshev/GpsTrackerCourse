8d5d2bc173443e169227f7fb27181ff9cde05a50
Sanek <s.groshev2010@yandex.ru>
2023-04-06 21:52:38 +0300

    access location 12

--- app/src/main/java/com/nosta/gpstrackercourse/fragments/MainFragment.kt
+++ app/src/main/java/com/nosta/gpstrackercourse/fragments/MainFragment.kt
@@ -1,21 +1,103 @@
 package com.nosta.gpstrackercourse.fragments
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
 import  android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.nosta.gpstrackercourse.R
 import com.nosta.gpstrackercourse.databinding.FragmentMainBinding
+import com.nosta.gpstrackercourse.utils.checkPermission
+import com.nosta.gpstrackercourse.utils.showToast
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        settingsOsm()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        initOSM()
+        registerPermission()
+        checkLocPermission()
+    }
+
+    private fun settingsOsm(){
+        Configuration.getInstance().load(
+            activity as AppCompatActivity,
+            activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+    }
+
+    private fun initOSM() = with(binding) {
+        map.controller.setZoom(20.0)
+//        map.controller.animateTo(GeoPoint(40.4167,-3.70325))
+        val mLogProvider = GpsMyLocationProvider(activity)
+        val mLockOverlay = MyLocationNewOverlay(mLogProvider, map)
+        mLockOverlay.enableMyLocation()
+        mLockOverlay.enableFollowLocation()
+        mLockOverlay.runOnFirstFix{
+            map.overlays.clear()
+            map.overlays.add(mLockOverlay)
+        }
+    }
+
+    private fun registerPermission() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                initOSM()
+            } else {
+                showToast("Вы не дали разрешения на использользование местоположения!")
+            }
+        }
+    }
+
+    private fun checkLocPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            checkPermissionAfter10()
+        } else {
+            checkPermissionBefore10()
+        }
+    }
+
+    private fun checkPermissionAfter10(){
+        if(checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+            initOSM()
+        } else {
+            pLauncher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ))
+        }
+    }
+
+    private fun checkPermissionBefore10(){
+        if(checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            initOSM()
+        } else {
+            pLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+        }
     }
 
     companion object {
--- app/src/main/java/com/nosta/gpstrackercourse/utils/Extentions.kt
+++ app/src/main/java/com/nosta/gpstrackercourse/utils/Extentions.kt
@@ -1,7 +1,10 @@
 package com.nosta.gpstrackercourse.utils
 
+import android.content.pm.PackageManager
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.nosta.gpstrackercourse.R
 
@@ -13,6 +16,10 @@ fun Fragment.openFragment(f: Fragment){
 }
 
 fun AppCompatActivity.openFragment(f: Fragment){
+//    Log.d("MyLog", "Frag name: ${supportFragmentManager.fragments.size}")
+    if (supportFragmentManager.fragments.isNotEmpty()) {
+        if (supportFragmentManager.fragments[0].javaClass == f.javaClass) return
+    }
     supportFragmentManager
         .beginTransaction()
         .setCustomAnimations(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
@@ -25,4 +32,11 @@ fun Fragment.showToast(s: String){
 
 fun AppCompatActivity.showToast(s: String){
     Toast.makeText(this,s,Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.checkPermission(p: String): Boolean {
+    return when(PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(activity as AppCompatActivity, p) -> true
+        else -> false
+    }
 }
